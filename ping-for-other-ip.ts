8b8bfabe2c2ed5ad75ba41efb117c10dda6418dd
David Ernst <git@dsernst.com>
2025-10-07 00:30:24 -0700

    nr other: Add error handling for OTHER_DEVICE Not Found

--- ping-for-other-ip.ts
+++ ping-for-other-ip.ts
@@ -1,21 +1,32 @@
 import { $ } from 'bun'
-import { CYAN, YELLOW, RESET } from './ansi_colors'
+import { CYAN, GRAY, YELLOW, RESET } from './ansi_colors'
 
+const TIMEOUT = 500
 const { OTHER_DEVICE_NAME, OTHER_IP } = process.env
-
-async function getIp(deviceName: string) {
-  const pingResponse = await $`ping -c 1 ${deviceName}`.text()
-  const firstLine = pingResponse.split('\n')[0]
-  const ip = firstLine.split('(')[1].split(')')[0]
-  return ip
-}
 
 if (!OTHER_DEVICE_NAME) {
   console.error('process.env.OTHER_DEVICE_NAME not set')
   process.exit(1)
 }
 
-const otherIp = await getIp(OTHER_DEVICE_NAME)
+async function getIp() {
+  const pingResponse = await $`ping -c 1 -W ${TIMEOUT} ${OTHER_DEVICE_NAME}`
+    .text()
+    .catch(() => {
+      // No network response
+      console.log(
+        `Timeout ${GRAY}${TIMEOUT}ms\n  ${YELLOW}${OTHER_DEVICE_NAME} ${GRAY}$OTHER_DEVICE_NAME${RESET} \n  404 NOT FOUND on network`
+      )
+      process.exit(1)
+    })
+
+  // If success, parse the IP from the ping response
+  const firstLine = pingResponse.split('\n')[0]
+  const ip = firstLine.split('(')[1].split(')')[0]
+  return ip
+}
+
+const otherIp = await getIp()
 console.log(
   `Found other (${OTHER_DEVICE_NAME}) at:\n  ${CYAN}${otherIp}${RESET}`
 )
