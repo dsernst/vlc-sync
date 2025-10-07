6205dc760223829d2a5151ef0883b4f178643b3d
David Ernst <git@dsernst.com>
2025-10-06 23:56:53 -0700

    nr other: Print current  value too

--- ping-for-other-ip.ts
+++ ping-for-other-ip.ts
@@ -1,7 +1,7 @@
 import { $ } from 'bun'
-import { CYAN, RESET } from './ansi_colors'
+import { CYAN, YELLOW, RESET } from './ansi_colors'
 
-const { OTHER_DEVICE_NAME } = process.env
+const { OTHER_DEVICE_NAME, OTHER_IP } = process.env
 
 async function getIp(deviceName: string) {
   const pingResponse = await $`ping -c 1 ${deviceName}`.text()
@@ -16,4 +16,12 @@ if (!OTHER_DEVICE_NAME) {
 }
 
 const otherIp = await getIp(OTHER_DEVICE_NAME)
-console.log(`Found other (${OTHER_DEVICE_NAME}) at: ${CYAN}${otherIp}${RESET}`)
+console.log(
+  `Found other (${OTHER_DEVICE_NAME}) at:\n  ${CYAN}${otherIp}${RESET}`
+)
+
+if (OTHER_IP !== otherIp)
+  console.log(
+    `\n⚠️  Mismatch: .env.local $OTHER_IP=\n  ${YELLOW}${OTHER_IP}${RESET}`
+  )
+else console.log('✅ .env.local $OTHER_IP set correctly')
